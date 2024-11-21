50d033bc94c3da63a4660603655b35fa265c7351
Ilya <simonewarlet@gmail.com>
2024-11-21 14:32:44 +0200

    Fix requires changing names of classes

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative('lib/Board')
-require_relative('lib/Robot')
+require_relative('lib/board')
+require_relative('lib/robot')
 
 # RobotController class that takes commands from user and make robot to move across the board
 class RobotController
